b599bdcc286a4b417c9cc73abf9b3c3204918a6f
yarebadekiruko11 <132581040+yarebadekiruko11@users.noreply.github.com>
2023-05-29 07:45:29 +0000

    [finish]5-13

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -19,16 +19,21 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.find(params[:id])
   end
 
+  def destroy
+    post_image = PostImage.find(params[:id])
+    post_image.destroy
+    redirect_to post_images_path
+
+
+  end
+
+
   # 投稿データのストロングパラメータ
   private
 
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
-
-
-
-
 
 
 end
