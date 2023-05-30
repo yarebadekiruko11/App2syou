c412928ef463970f45b15435eeb3cb26d6995951
yarebadekiruko11 <132581040+yarebadekiruko11@users.noreply.github.com>
2023-05-30 12:20:47 +0000

    [finish]5-18

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-
-  root to: 'homes#top'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  root to: 'homes#top'
+  get "/homes/about" => "homes#about", as: "about"
+
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
+
+      # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   resources :users, only: [:show, :edit, :update]
 
- get "/homes/about" => "homes#about", as: "about"
+
 
 end
