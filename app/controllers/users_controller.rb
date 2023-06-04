6302f404903a001a117dc6bf6230221a102bc7f8
yarebadekiruko11 <132581040+yarebadekiruko11@users.noreply.github.com>
2023-06-04 13:37:17 +0000

    [start]user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,25 @@ class UsersController < ApplicationController
   end
 
   def edit
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
+
     @user = User.find(params[:id])
 
   end
 
   def update
+
+    user = User.find(params[:id])
+    unless user.id == cureent_user.id
+      redirect_to post_images_path
+    end
+
+
+
+
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
